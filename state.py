10b2f809e76934b8c641746090d81e23342f64c1
hurricaneJoef <33767776+hurricaneJoef@users.noreply.github.com>
2019-08-03 20:16:17 -0400

    Update state.py

--- state.py
+++ state.py
@@ -378,7 +378,7 @@ class statematch:
     def greenlight(self,img):
         box=cd_color_segmentation(img,colorval=[[63, 119, 75],[98, 255, 100]])#[21, 129, 63],[83, 255, 205]  [ 17, 175, 23],[ 82, 255, 148]
         if box[1][1]<150:
-            if self.size_calc(box)>400:
+            if self.size_calc(box)>90:
                 return True
         return False
     def size_calc(self,box):
